b1d26c368193032aa27cbcbd48ca79b18d788bd4
anbeoo1112 <lean1112work1@gmail.com>
2023-10-24 08:34:17 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
 // coding implementation
+var email = document.getElementById(email);
+var comment = document.getElementById(com);
+var rate = document.getElementById(rate);
+var btn 
+function 
 // end coding implementation
